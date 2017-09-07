b040a2def9bf6d3f63f2c6de342497bb90468cf2
irvin <ecole.venansol@gmail.com>
2017-09-07 08:58:53 +0200

    commit

--- src/Venansol/SiteBundle/Controller/DefaultController.php
+++ src/Venansol/SiteBundle/Controller/DefaultController.php
@@ -8,7 +8,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/index" ,name="index")
+     * @Route("/" ,name="index")
      */
     public function indexAction()
     {
